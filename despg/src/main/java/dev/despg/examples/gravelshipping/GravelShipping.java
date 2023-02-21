8d012310a0c866c0ec893311e4773cc916ddda83
ChristianMinich <chris.minich.cm@gmail.com>
2023-02-21 15:37:29 +0100

    ..

--- despg/src/main/java/dev/despg/examples/gravelshipping/GravelShipping.java
+++ despg/src/main/java/dev/despg/examples/gravelshipping/GravelShipping.java
@@ -128,7 +128,7 @@ public class GravelShipping extends Simulation
 				+ " #Trucks Loading Queue: " + numberOfTrucksLoadingQueue + ", # Trucks Weighing Queue: " + numberOfTrucksWeighingQueue + " ," + numberOfTrucksUnloadingQueue);
 	}
 	
-	public static SimulationObject getFirstWeighingStation()
+	public static SimulationObject getFirstWeighingStation(ArrayList<SimulationObject> FirstWeighingStation)
 	{
 		return FirstWeighingStation.get(0);
 	}
